730a61f64f165194f51fec3c278066071a976d9f
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-06 17:05:13 +0200

    create animated portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
+};
+
+export default App;
