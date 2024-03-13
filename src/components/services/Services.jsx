ec502561398f30b893d303052d010c7b852ae411
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-13 13:54:44 +0200

    create services component

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,70 @@
+import "./services.scss";
+import { motion } from "framer-motion";
+
+const Services = () => {
+  return (
+    <motion.div className="services">
+      <motion.div className="textContainer">
+        <p>
+          I focus on helping your brand grow
+          <br /> and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="titleContainer">
+        <div className="title">
+          <img src="/public/people.webp" alt="" />
+          <h1>
+            <b>Unique</b> Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <b>For Your</b> Business.
+          </h1>
+          <button>WHAT WE DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer">
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
+            atiquam consectetur expedita magni eius ex corrupti anini! Ad ne
+            pariatur quae nottitia libero repel tat explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
+            atiquam consectetur expedita magni eius ex corrupti anini! Ad ne
+            pariatur quae nottitia libero repel tat explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
+            atiquam consectetur expedita magni eius ex corrupti anini! Ad ne
+            pariatur quae nottitia libero repel tat explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
+            atiquam consectetur expedita magni eius ex corrupti anini! Ad ne
+            pariatur quae nottitia libero repel tat explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default Services;
