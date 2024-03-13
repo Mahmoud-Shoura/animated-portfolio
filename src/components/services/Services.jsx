648e36fdaf624b6b68ed16c725786b638b57bb75
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-03-13 15:01:52 +0200

    end services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,32 +1,69 @@
+import { useRef } from "react";
 import "./services.scss";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
 
 const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      //   animate="animate"
+      //   whileInView="animate"
+      ref={ref}
+      animate={isInView && "animate"}
+    >
+      <motion.div className="textContainer" variants={variants}>
         <p>
           I focus on helping your brand grow
           <br /> and move forward
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <img src="/public/people.webp" alt="" />
           <h1>
-            <b>Unique</b> Ideas
+            <motion.b whileHover={{ color: " orange " }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <b>For Your</b> Business.
+            <motion.b whileHover={{ color: " orange " }}>For Your</motion.b>{" "}
+            Business.
           </h1>
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
-        <div className="box">
+      <motion.div className="listContainer" variants={variants}>
+        <motion.div
+          className="box"
+          whileHover={{
+            background: "lightgray",
+            color: "black",
+          }}
+        >
           <h2>Branding</h2>
           <p>
             Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
@@ -34,8 +71,14 @@ const Services = () => {
             pariatur quae nottitia libero repel tat explicabo maiores?
           </p>
           <button>Go</button>
-        </div>
-        <div className="box">
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{
+            background: "lightgray",
+            color: "black",
+          }}
+        >
           <h2>Branding</h2>
           <p>
             Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
@@ -43,8 +86,14 @@ const Services = () => {
             pariatur quae nottitia libero repel tat explicabo maiores?
           </p>
           <button>Go</button>
-        </div>
-        <div className="box">
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{
+            background: "lightgray",
+            color: "black",
+          }}
+        >
           <h2>Branding</h2>
           <p>
             Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
@@ -52,8 +101,14 @@ const Services = () => {
             pariatur quae nottitia libero repel tat explicabo maiores?
           </p>
           <button>Go</button>
-        </div>
-        <div className="box">
+        </motion.div>
+        <motion.div
+          className="box"
+          whileHover={{
+            background: "lightgray",
+            color: "black",
+          }}
+        >
           <h2>Branding</h2>
           <p>
             Lorem ipsum dolor sit consectetur adipisicing NOStnn enim nisi
@@ -61,7 +116,7 @@ const Services = () => {
             pariatur quae nottitia libero repel tat explicabo maiores?
           </p>
           <button>Go</button>
-        </div>
+        </motion.div>
       </motion.div>
     </motion.div>
   );
